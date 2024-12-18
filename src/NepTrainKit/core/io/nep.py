d7217a6e314c3330327d62644224ae5c9bb639bf
aboy <1747193328@qq.com>
2024-12-18 22:53:05 +0800

    1.5.5

--- src/NepTrainKit/core/io/nep.py
+++ src/NepTrainKit/core/io/nep.py
@@ -64,17 +64,9 @@ class NepTrainResultData(QObject):
         self.virial_out_path = virial_out_path
 
         atoms_num_list = read_atom_num_from_xyz(self.data_xyz_path)
-        proc = mp.QtProcess( )
-
-        structures=proc.callObj(Structure.read_multiple,(self.data_xyz_path,),{} ,_timeout=60*60 ,_returnType="value" )
-
-
+        structures = Structure.read_multiple(self.data_xyz_path)
 
         self._atoms_dataset=StructureData(structures)
-        proc_structures = proc.transfer(structures)
-
-        nep3 = proc.callObj(Nep3Calculator, (self.nep_txt_path.as_posix(),), {}, timeout=60 * 60)
-
         nep_in= read_nep_in(self.nep_txt_path.with_name("nep.in"))
 
         if (not check_fullbatch(nep_in,len(structures))
@@ -87,10 +79,8 @@ class NepTrainResultData(QObject):
         ])):
 
 
-                nep_potentials_array,nep_forces_array,nep_virials_array  = nep3.calculate(proc_structures ,_returnType='value',_timeout=60*60 )
 
-
-                # nep_potentials_array,nep_forces_array,nep_virials_array = process_calculate(self.nep_txt_path.as_posix(), structures)
+                nep_potentials_array,nep_forces_array,nep_virials_array = process_calculate(self.nep_txt_path.as_posix(), structures)
                 energy_array=np.column_stack([nep_potentials_array/atoms_num_list,[structure.per_atom_energy for structure in structures]])
                 try:
 
@@ -158,11 +148,7 @@ class NepTrainResultData(QObject):
 
             self._virial_dataset = NepPlotData([],title="virial")
 
-
-        desc_array = nep3.get_descriptors(structures,_returnType='value',_timeout=60*60 )
-
-
-
+        desc_array=process_get_descriptors(self.nep_txt_path.as_posix(), structures)
         desc_array = pca(desc_array,2)
 
         # desc_array=np.array([])
