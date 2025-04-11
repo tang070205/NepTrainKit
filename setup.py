cfcd4430870b94c55a44f066c77e7410d59ceb92
tang070205 <tang070205@proton.me>
2025-04-11 17:21:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,19 +23,21 @@ pybind11_include = pybind11.get_include()
 # 设定编译选项
 extra_link_args = []
 extra_compile_args = []
-data_files = []
+data_files = []  # 默认初始化为空列表，避免未定义错误
+
 # 检查平台并设置相应的 OpenMP 编译标志
 if sys.platform == "win32":
     mingw_bin_dir = os.path.abspath("src/nep_cpu")
     os.environ['CC'] = os.path.join(mingw_bin_dir, 'gcc.exe')
     os.environ['CXX'] = os.path.join(mingw_bin_dir, 'g++.exe')
-    data_files = [('bin', [os.path.join(mingw_bin_dir, 'gcc.exe'),
-        os.path.join(mingw_bin_dir, 'g++.exe'),
-        os.path.join(mingw_bin_dir, 'libgcc_s_seh-1.dll'),
-        os.path.join(mingw_bin_dir, 'libstdc++-6.dll'),
-        os.path.join(mingw_bin_dir, 'libwinpthread-1.dll'),
-        os.path.join(mingw_bin_dir, 'libgomp-1.dll')])]
-
+    data_files = [('bin', [  # 仅 Windows 平台需要 data_files
+            os.path.join(mingw_bin_dir, 'gcc.exe'),
+            os.path.join(mingw_bin_dir, 'g++.exe'),
+            os.path.join(mingw_bin_dir, 'libgcc_s_seh-1.dll'),
+            os.path.join(mingw_bin_dir, 'libstdc++-6.dll'),
+            os.path.join(mingw_bin_dir, 'libwinpthread-1.dll'),
+            os.path.join(mingw_bin_dir, 'libgomp-1.dll')
+        ])]
     # 使用 MinGW 的 GCC 编译器
     extra_compile_args.append('-fopenmp')
     extra_compile_args.append('-O3')
@@ -45,17 +47,14 @@ if sys.platform == "win32":
     extra_link_args.append('-O3')
     extra_link_args.append('-std=c++11')
 elif sys.platform == "darwin":
-    # 对于 macOS 和 Clang 使用 -fopenmp 编译标志
-    # Clang 好像不支持openmp 先注释掉
-    # extra_compile_args.append('-fopenmp')
-
+    # macOS 使用 Clang（默认不支持 OpenMP）
     extra_compile_args.append('-O3')
     extra_compile_args.append('-std=c++11')
 
     extra_link_args.append('-O3')
     extra_link_args.append('-std=c++11')
 else:
-    # 对于 Linux 和 GCC 使用 -fopenmp 编译标志
+    # Linux 使用 GCC + OpenMP
     extra_compile_args.append('-fopenmp')
     extra_compile_args.append('-O3')
     extra_compile_args.append('-std=c++11')
@@ -102,6 +101,6 @@ setup(
     author="Chen Cheng bing",
     cmdclass={'build_ext': BuildExt},
     ext_modules=ext_modules,
-    data_files=data_files
+    data_files=data_files,  # 现在 data_files 始终有定义
     zip_safe=False,
 )
